ec73c16ec03928f23a972209447dfcc1ff4d7eab
Naufal Muhammad Farras <opayhero@gmail.com>
2024-07-30 18:45:58 +0700

    Add Runway info and fixing json parser

--- weather_parsers.py
+++ weather_parsers.py
@@ -10,7 +10,17 @@ class WeatherParser:
         """
         Convert a Metar object to a dictionary.
         """
-    
+        
+        def wind_to_dict(wind):
+            if wind is None:
+                return None
+            return {
+                "speed": wind.speed,
+                "direction": wind.direction,
+                "gust": wind.gust,
+                "degrees": wind.degrees,
+                "unit": wind.unit
+            }
         
         return {
             "station": metar.station,
@@ -22,10 +32,14 @@ class WeatherParser:
                 "direction": metar.wind.direction,
                 "gust": metar.wind.gust,
                 "degrees": metar.wind.degrees,
-                "unit": metar.wind.unit
+                "unit": metar.wind.unit,
+                "min_variation" : metar.wind.min_variation if metar.wind.min_variation else None,
+                "max_variation" : metar.wind.max_variation if metar.wind.max_variation else None
             },
             "visibility": {
-                "distance": metar.visibility.distance
+                "distance": metar.visibility.distance,
+                "min_distance": metar.visibility.min_distance,
+                "min_direction": metar.visibility.min_direction
             },
             "clouds": [
                 {
@@ -41,6 +55,43 @@ class WeatherParser:
                     "phenomenons": [str(p) for p in condition.phenomenons]
                 } for condition in metar.weather_conditions
             ],
+            "trends": [
+                {
+                    "type": str(trend.type),
+                    "wind": wind_to_dict(trend.wind),
+                    "visibility": {
+                        "distance": trend.visibility.distance if trend.visibility else None
+                    },
+                    "clouds": [
+                        {
+                            "height": cloud.height,
+                            "quantity": str(cloud.quantity),
+                            "type": str(cloud.type)
+                        } for cloud in trend.clouds
+                    ],
+                    "weather_conditions": [
+                        {
+                            "intensity": str(condition.intensity),
+                            "descriptive": str(condition.descriptive),
+                            "phenomenons": [str(p) for p in condition.phenomenons]
+                        } for condition in trend.weather_conditions
+                    ],
+                    "cavok":trend.cavok if trend.cavok else False
+                } for trend in metar.trends
+            ],
+            "runways_info": [
+                {
+                    "name": runway_info.name,
+                    "min_range": runway_info.min_range,
+                    "max_range": runway_info.max_range,
+                    "trend": runway_info.trend if runway_info.trend else "",
+                    "indicator": runway_info.indicator if runway_info.indicator else "",
+                }for runway_info in metar.runways_info
+            ],
+            "flags": list(metar.flags),
+            "wind_shear":metar.wind_shear if metar.wind_shear else None,
+            "cavok":metar.cavok if metar.cavok else False,
+            "remark":metar.remark if metar.remark else None,
             "temperature": None if metar.temperature is None else metar.temperature,
             "dew_point": None if metar.dew_point is None else metar.dew_point,
             "altimeter": metar.altimeter,
@@ -127,7 +178,8 @@ class WeatherParser:
                             "descriptive": str(condition.descriptive),
                             "phenomenons": [str(p) for p in condition.phenomenons]
                         } for condition in trend.weather_conditions
-                    ]
+                    ],
+                    "cavok":trend.cavok if trend.cavok else False
                 } for trend in taf.trends
             ],
             "flags": list(taf.flags),
